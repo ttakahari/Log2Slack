b60e870ffebaf864f1c576a8576d6776a5df5dbb
t.takahari <taiga1018@gmail.com>
2018-08-19 17:54:10 +0900

    add log4net project.

---
+++ src/Log2Slack.Log4Net/SlackAppender.cs
@@ -0,0 +1,52 @@
+﻿using log4net.Appender;
+using log4net.Core;
+using SlackSharp;
+using SlackSharp.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Log2Slack.Log4Net
+{
+    public class SlackAppender : AppenderSkeleton
+    {
+        private IWebHookClient _client;
+
+        public string WebHookUrl { get; set; }
+
+        public Type SerializerType { get; set; }
+
+        public string Channel { get; set; }
+
+        public string Username { get; set; }
+
+        public string IconUrl { get; set; }
+
+        public string IconEmoji { get; set; }
+
+        public override void ActivateOptions()
+        {
+            base.ActivateOptions();
+
+            if (string.IsNullOrEmpty(WebHookUrl)) throw new LogException($"{nameof(WebHookUrl)} must not be empty or null.");
+            if (SerializerType == null) throw new LogException($"{nameof(SerializerType)} must not be empty or null.");
+
+            var serializer = Activator.CreateInstance(SerializerType) as IHttpContentJsonSerializer;
+
+            if (serializer == null)
+            {
+                throw new LogException($"{nameof(SerializerType)} must implements {typeof(IHttpContentJsonSerializer).FullName}.");
+            }
+
+            _client = new WebHookClient(serializer);
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            var message = RenderLoggingEvent(loggingEvent);
+
+            _client
+                .SendAsync(WebHookUrl, new Payload { Channel = Channel, Username = Username, IconEmoji = IconEmoji, IconUrl = IconUrl, Text = message })
+                .ContinueWith(t => ErrorHandler.Error(t.Exception.ToString()), TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}
